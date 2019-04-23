f1458fa202d1707d9a4e4d489356d9a86c13bbc1
Mattias Jönsson <matjon4@kth.se>
2019-04-23 14:27:54 +0200

    Extended code to d-dimensions and added README

---
+++ README.md
@@ -0,0 +1,22 @@
+# Stochastic differential equations lab
+This repository contains code for the course SF2522.
+
+## Requirements
+Make sure to have installed:
+
+* Python 3.6 or later
+* pip 3
+
+## Installation
+Install the requirements
+```sh
+$ pip install -r requirements.txt
+```
+
+## Homework 4
+To run the code use
+```sh
+$ python tf_lab.py
+```
+To change the number of dimensions edit the variable `d` on line 6. 
+
--- tf_lab.py
+++ tf_lab.py
@@ -2,19 +2,41 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 100
+# Dimension of the target f: R^d -> R
+d = 2
+
+# Number of training points
+N = 1000
+
+# Number of nodes in the hidden layer
 K = 10
+
+# Number of training steps
 M = 40000
+
+# Learning rate
 dt = 0.005
+
+# The total training time
 T = M*dt
 
-inp = tf.placeholder(dtype = tf.float32, shape = (None, 1))
+# Create a placeholder for the input
+inp = tf.placeholder(dtype=tf.float32, shape=(None, d))
 
-x_training = np.random.uniform(-4,4,size=(N,1))
-x_test = np.random.uniform(-4,4,size=(100,1))
-target_fcn = tf.square(tf.abs(inp - 0.5))
+# Create mock training and testing data
+x_training = np.random.uniform(-4, 4, size=(N, d))
+x_test = np.random.uniform(-4, 4, size=(100, d))
+
+# Create the mock target
+target_fcn = tf.reshape(tf.square(tf.norm(inp - 0.5, axis=1)), (-1, 1))
+
 
 def neural_network_model(inp):
+    """
+    Generate the neural network
+    :param inp: the input tensor
+    :return: the neural network tensor
+    """
     initializer = tf.initializers.random_normal
     activation_fcn_1 = tf.nn.sigmoid
     activation_fcn_output = None
@@ -32,7 +54,13 @@ def neural_network_model(inp):
                              kernel_initializer=initializer())
     return output
 
+
 def train_neural_network(sess):
+    """
+    Train a neural network with mean square loss
+    :param sess: the session to use during training
+    :return: the trained neural network and the expected errors
+    """
     alpha = neural_network_model(inp)
     cost = tf.reduce_mean(tf.square(alpha - target_fcn))
     optimizer = tf.train.GradientDescentOptimizer(dt)
@@ -44,31 +72,37 @@ def train_neural_network(sess):
     for m in range(0, M):
         n = np.random.randint(0, N)
         x_in = x_training[n:n + 1]
-        sess.run(train, feed_dict = {inp:x_in})
+        sess.run(train, feed_dict={inp: x_in})
         
         if m % int(M/100) == 0:
-            E_1.append(cost.eval(feed_dict={inp:x_test}))
+            E_1.append(cost.eval(feed_dict={inp: x_test}))
         if m % int(M/10) == 0:
             print(m)
             
     return alpha, E_1
 
-pts = np.linspace(-4,4,300).reshape(-1,1)
 
+# Plot the target function and the neural network in a two dimensional plane.
+# Create data points for the axis we wish to plot
+one_d_pts = np.linspace(-4, 4, 300)
+
+# Set all other coordinates to zero
+pts = np.vstack([one_d_pts, np.zeros((d - 1, 300))]).T
+
+# Run the training procedure and retrieve values of the target and the network
 with tf.Session() as sess:
     alpha, E_1 = train_neural_network(sess)
-    training_data_vals = target_fcn.eval(feed_dict={inp:x_training})
-    target_fcn_vals = target_fcn.eval(feed_dict={inp:pts})
-    alpha_vals = alpha.eval(feed_dict={inp:pts})
+    target_fcn_vals = target_fcn.eval(feed_dict={inp: pts})
+    alpha_vals = alpha.eval(feed_dict={inp: pts})
 
-plt.figure('Learned function', figsize=(15,10));
-plt.plot(x_training, training_data_vals, 'o', label='Training data')
-plt.plot(pts, target_fcn_vals, label='Target function')
-plt.plot(pts, alpha_vals, label='alpha')
+# Plot the data
+plt.figure('Learned function')
+plt.plot(one_d_pts, target_fcn_vals, label='Target function')
+plt.plot(one_d_pts, alpha_vals, label=r'$\alpha$')
 plt.legend()
 plt.show()
 
-plt.figure('Test error', figsize=(15,10));
-plt.semilogy(np.linspace(0, T, len(E_1)), E_1, label='E_1')
+plt.figure('Test error')
+plt.semilogy(np.linspace(0, T, len(E_1)), E_1, label='$E_1$')
 plt.legend()
 plt.show()
